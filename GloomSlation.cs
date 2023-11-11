cd16d2c029b01e3e7153e7047a3e4856876af64c
nikvoid <77514082+nikvoid@users.noreply.github.com>
2023-11-11 23:59:58 +0200

    initial texture patching: support for anything that has Renderer or Image components

--- GloomSlation.cs
+++ GloomSlation.cs
@@ -12,6 +12,7 @@ using System.Reflection;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 [assembly: MelonInfo(typeof(GloomSlation.GloomSlation), "GloomSlation", "0.1.0", "pipo, nikvoid")]
@@ -31,13 +32,30 @@ namespace GloomSlation
             return inst == null || inst == string.Empty;
         }
 
+        /// Try to add marker component on GameObject
+        public static bool SetMarker<T>(this GameObject obj) where T: Marker {
+            // Already set
+            if (obj.GetComponent<T>() != null) {
+                return false;
+            }
+
+            // Set mark
+            obj.AddComponent<T>();
+            return true;
+        }
     }
 
-    public class ProcessedMarker: MonoBehaviour {}
+    public class Marker: MonoBehaviour {}
+
+    // Classes for marking translated objects
+    public class TextProcessedMarker: Marker {}
+    public class SpriteProcessedMarker: Marker {}
+    public class TextureProcessedMarker: Marker {}
 
     public class GloomSlation : MelonMod
     {
         private readonly Dictionary<string, TMP_FontAsset> fontMap = new Dictionary<string, TMP_FontAsset>();
+        private readonly Dictionary<string, Texture2D> textures = new Dictionary<string, Texture2D>();
        
         private static readonly string modPath = "Mods\\GloomSlation";
 
@@ -92,14 +110,36 @@ namespace GloomSlation
                     DebugMsg($"{k} -> {v}");
                 }
             }
+
+            // Load textures
+            var texPath = Path.Combine(langPath, "Textures");
+            if (Directory.Exists(texPath)) {
+                foreach(var path in Directory.EnumerateFiles(texPath)) {
+                    var name = Path.GetFileNameWithoutExtension(path);
+                    if (Path.GetExtension(path) != ".png") {
+                        continue;
+                    }
+
+                    
+                    var imageData = File.ReadAllBytes(path); 
+                    var tex = new Texture2D(1, 1);
+                    if (!ImageConversion.LoadImage(tex, imageData, false)) {
+                        MelonLogger.Error($"Could not load texture {name}");
+                    } else {
+                        textures.Add(name, tex);
+                        DebugMsg($"Loaded texture {name}");                    
+                    }
+                }
+            }
         }
         
+        /// Log message only if debugMode active
         public void DebugMsg(object msg) {
             if (debugMode) {
                 MelonLogger.Msg(msg);
             }
         }
-        
+
         /// Patch all scene's GameObjects
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
@@ -115,90 +155,131 @@ namespace GloomSlation
         public static string ConstructLocaleKey(string objName, string origText) {
             return notIdentRex.Replace($"{objName}@{origText}", "_");
         }
+
+        /// Patch single TextMeshProUGUI
+        private void PatchTextMesh(TextMeshProUGUI tmp) {
+            // Remap fonts
+            if (tmp.font != null)
+            {
+                TMP_FontAsset font;
+                if (fontMap.TryGetValue(tmp.font.name, out font))
+                {
+                    tmp.font = font;
+                }
+                else if (fontMap.TryGetValue("FALLBACK_FONT", out font))
+                {
+                    DebugMsg($"mapping {tmp.font.name} -> FALLBACK");
+                    tmp.font = font;
+                }
+            }
+
+            var tmpObj = tmp.gameObject;
+            var component = tmpObj.GetComponent<TextBehaviour>();
+            var localizedText = tmpObj.GetComponent<LocalizedText>();
+            
+            // For every object that doesn't have localization component or key, 
+            // generate key and try to get localization
+            if (!tmp.text.NullOrEmpty()
+                && (component == null || localizedText == null || !localizedText.enabled)
+            ) {
+                var localeKey = ConstructLocaleKey(tmpObj.name, tmp.text);
+                    
+                DebugMsg($"Found unlocalized text: {localeKey} = \"{tmp.text}\";");
+
+                // Most of such texts are related to menu, so let's store there all of them
+                if(GameManager.LanguageManager.TryGetLocalizedContent(
+                    LanguageDataTypes.Menus,
+                    localeKey,
+                    out string localized
+                )) {
+                    DebugMsg("Found localization!");
+                    tmp.text = localized;
+
+                    // This prevents anything from changing our text back
+                    tmp.OnPreRenderText += (ti) => {
+                        // Prerender triggered by our action
+                        if (ti.textComponent.text == localized) {
+                            return;
+                        }    
+                    
+                        var newLocaleKey = ConstructLocaleKey(tmpObj.name, ti.textComponent.text);
+                        DebugMsg($"Unlocalized change: {localeKey} -> {newLocaleKey}: \"{ti.textComponent.text}\"");
+                        if(GameManager.LanguageManager.TryGetLocalizedContent(
+                            LanguageDataTypes.Menus,
+                            newLocaleKey,
+                            out string localizedNew
+                        )) {
+                            // Update text in case there's something localized
+                            DebugMsg("Found new localization!");
+                            ti.textComponent.text = localizedNew;                               
+                            localized = localizedNew;
+                            ti.textComponent.ForceMeshUpdate(true, true);
+                        } else {
+                            // Write previous value back
+                            ti.textComponent.text = localized;
+                            ti.textComponent.ForceMeshUpdate(true, true);
+                        }                        
+                    };
+                }
+            }
+
+            // Force-enable components
+            if (component != null) {
+                component.enabled = true;
+            }
+        }
+
+        /// Patch main texture in renderer's material
+        public void PatchRenderer(Renderer rend) {
+            var mat = rend.material;
+            if (mat == null) {
+                return;
+            }
+            foreach(var prop in mat.GetTexturePropertyNames()) {
+                if (prop != "_MainTex") {
+                    continue;
+                }
+                
+                var tex = mat.GetTexture(prop);
+                if (tex != null && tex.name != null && textures.TryGetValue(tex.name, out Texture2D newTex)) {
+                    DebugMsg($"Patching texture {tex.name} in {mat.name}");
+                    mat.SetTexture(prop, newTex);
+                }
+            }
+        }
         
-        /// Patch single GameObject
+        /// Patch image sprite
+        public void PatchImage(Image img) {
+            if (textures.TryGetValue(img.mainTexture.name, out Texture2D nTex)) {
+                var sprite = Sprite.Create(nTex, img.sprite.rect, img.sprite.pivot);
+                DebugMsg($"Patching sprite in {img.name}: {img.mainTexture.name}");
+                img.sprite = sprite;
+            }
+        }
+        
+        /// Patch GameObject and all of its childen
         public void PatchGameObject(GameObject obj)
         {
-            // Recursively patch all texts found
+            // Patch all nested texts found
             foreach (var tmp in obj.GetComponentsInChildren<TMPro.TextMeshProUGUI>())
             {
-                var tmpObj = tmp.gameObject;
-
                 // If already processed, skip
-                if (tmpObj.GetComponent<ProcessedMarker>() != null) {
-                    return;
+                if (tmp.gameObject.SetMarker<TextProcessedMarker>()) {
+                    PatchTextMesh(tmp);
                 }
-
-                // Mark objects as already processed
-                tmpObj.AddComponent<ProcessedMarker>();
-                
-                // Remap fonts
-                if (tmp.font != null)
-                {
-                    TMP_FontAsset font;
-                    if (fontMap.TryGetValue(tmp.font.name, out font))
-                    {
-                        tmp.font = font;
-                    }
-                    else if (fontMap.TryGetValue("FALLBACK_FONT", out font))
-                    {
-                        DebugMsg($"mapping {tmp.font.name} -> FALLBACK");
-                        tmp.font = font;
-                    }
+            }
+            // Patch all textures in materials
+            foreach (var rend in obj.GetComponentsInChildren<Renderer>())
+            {
+                if (rend.gameObject.SetMarker<TextureProcessedMarker>()) {
+                    PatchRenderer(rend);
                 }
-
-                var component = tmpObj.GetComponent<TextBehaviour>();
-                var localizedText = tmpObj.GetComponent<LocalizedText>();
-                
-                // For every object that doesn't have localization component or key, 
-                // generate key and try to get localization
-                if (!tmp.text.NullOrEmpty()
-                    && (component == null || localizedText == null || !localizedText.enabled)
-                ) {
-                    var localeKey = ConstructLocaleKey(tmpObj.name, tmp.text);
-                        
-                    DebugMsg($"Found unlocalized text: {localeKey} = \"{tmp.text}\";");
-
-                    // Most of such texts are related to menu, so let's store there all of them
-                    if(GameManager.LanguageManager.TryGetLocalizedContent(
-                        LanguageDataTypes.Menus,
-                        localeKey,
-                        out string localized
-                    )) {
-                        DebugMsg("Found localization!");
-                        tmp.text = localized;
-
-                        // This prevents anything from changing our text back
-                        tmp.OnPreRenderText += (ti) => {
-                            // Prerender triggered by our action
-                            if (ti.textComponent.text == localized) {
-                                return;
-                            }    
-                        
-                            var newLocaleKey = ConstructLocaleKey(tmpObj.name, ti.textComponent.text);
-                            DebugMsg($"Unlocalized change: {localeKey} -> {newLocaleKey}: \"{ti.textComponent.text}\"");
-                            if(GameManager.LanguageManager.TryGetLocalizedContent(
-                                LanguageDataTypes.Menus,
-                                newLocaleKey,
-                                out string localizedNew
-                            )) {
-                                // Update text in case there's something localized
-                                DebugMsg("Found new localization!");
-                                ti.textComponent.text = localizedNew;                               
-                                localized = localizedNew;
-                                ti.textComponent.ForceMeshUpdate(true, true);
-                            } else {
-                                // Write previous value back
-                                ti.textComponent.text = localized;
-                                ti.textComponent.ForceMeshUpdate(true, true);
-                            }                        
-                        };
-                    }
-                }
-
-                // Force-enable components
-                if (component != null) {
-                    component.enabled = true;
+            }
+            // Patch all sprites
+            foreach (var img in obj.GetComponentsInChildren<Image>())
+            {
+                if (img.gameObject.SetMarker<SpriteProcessedMarker>()) {
+                    PatchImage(img);
                 }
             }
         }
